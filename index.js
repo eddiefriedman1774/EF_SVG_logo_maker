d2a14dad61f1777ee405442215440dac52aaf818
eddiefriedman1774 <eddiefriedman1@gmail.com>
2023-06-11 17:59:07 -0400

    added svg file and managed to open via live server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // External packages
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
+
+
 // Inquirer prompts for userResponses
 const questions = [
 
@@ -27,6 +29,17 @@ const questions = [
         name: 'color'
     }
 ];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your svg file has been generated")
+    });
+}
+
 async function init() {
     try {
 
@@ -40,4 +53,17 @@ async function init() {
         console.log(error);
     }
 };
-init();
+
+function generateSVG (characters, textColor, shapeType, shapeColor) {
+    svg = `<?xml version="1.0" standalone="no"?>`+"\n"
+    svg += `<svg width="300" height="200" version="1.1"`
+    svg += ` xmlns="http://www.w3.org/2000/svg">`+"\n"
+    svg += ` <rect x="10" y="10" width="30" height="30" stroke="black" />`+"\n"
+    svg += `</svg>`
+    return svg 
+};
+
+// init();
+
+fileData = generateSVG();
+writeToFile ("SVG_LOGO.svg", fileData);
